2c0985ac60b986bb226d1c9d87d484b9cd869e79
Romuald Brillout <git@brillout.com>
2025-08-18 21:03:09 +0200

    fix: avoid Vite type version mismatch

--- packages/vike-server/src/plugin/index.ts
+++ packages/vike-server/src/plugin/index.ts
@@ -4,13 +4,17 @@ import { commonConfig } from './plugins/commonConfig.js'
 import { devServerPlugin } from './plugins/devServerPlugin.js'
 import { serverEntryPlugin } from './plugins/serverEntryPlugin.js'
 import { standalonePlugin } from './plugins/standalonePlugin.js'
+import type { Plugin } from 'vite'
 
-function vikeServer() {
-  return [
+type PluginInterop = Record<string, unknown> & { name: string }
+// Return `PluginInterop` instead of `Plugin` to avoid type mismatch upon different Vite versions
+function vikeServer(): PluginInterop[] {
+  const plugins: Plugin[] = [
     //
     ...commonConfig(),
     ...serverEntryPlugin(),
     devServerPlugin(),
     standalonePlugin()
   ]
+  return plugins as any
 }
