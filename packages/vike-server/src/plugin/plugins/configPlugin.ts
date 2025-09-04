5480c94b75c1b81584c0a09b65bb321cbf54076d
Joël Charles <joel.charles91@gmail.com>
2025-09-04 10:25:32 +0200

    chore: move settings from vpv

--- packages/vike-server/src/plugin/plugins/configPlugin.ts
+++ packages/vike-server/src/plugin/plugins/configPlugin.ts
@@ -6,12 +6,24 @@ export function configPlugin(): Plugin {
     config() {
       return {
         resolve: {
+          // Contains virtual modules
           noExternal: ['vike-server']
         },
         ssr: {
           optimizeDeps: {
+            // Those are ESM packages, no need to optimize them
             exclude: ['vike-server', 'vike'],
+            // Still optimize this dual-format dependency
             include: ['vike > @brillout/require-shim']
+          }
+        },
+        photon: {
+          // Vike does not support declaring its routes as individual Universal Handlers.
+          // Settings `framework: false` signals deployment targets that each route bundles the whole app.
+          // If it were `framework: true` (default), Photon considers that each route points to a Universal Handler,
+          // allowing deployment targets to code-split the app, and provide better error messages when using the `addPhotonEntry` api.
+          codeSplitting: {
+            framework: false
           }
         }
       }
