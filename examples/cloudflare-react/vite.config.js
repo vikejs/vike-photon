7168d2c4cb3650766e2e397c16b2a9fdbb217c17
Joël Charles <joel.charles91@gmail.com>
2025-09-08 16:30:55 +0200

    chore: clean example

--- examples/cloudflare-react/vite.config.js
+++ examples/cloudflare-react/vite.config.js
@@ -1,8 +1,7 @@
 import { cloudflare } from "@photonjs/cloudflare/vite";
 import react from "@vitejs/plugin-react";
 import vike from "vike/plugin";
-import vikePhoton from "vike-photon/plugin";
 
 export default {
-  plugins: [react(), vike(), vikePhoton(), cloudflare()],
+  plugins: [react(), vike(), cloudflare()],
 };
