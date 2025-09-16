b8c3b389e76930edc9e386e546a3e03b98504904
Joël Charles <joel.charles91@gmail.com>
2025-09-16 14:15:03 +0200

    feat: complete vercel integration

--- packages/vike-photon/src/plugin/plugins/targets.ts
+++ packages/vike-photon/src/plugin/plugins/targets.ts
@@ -1,5 +1,8 @@
+import path from "node:path";
 import { isPackageExists } from "local-pkg";
-import type { PluginOption } from "vite";
+import { getVikeConfig } from "vike/plugin";
+import { normalizePath, type PluginOption } from "vite";
+import { getVercelAPI } from "vite-plugin-vercel/api";
 import { setTargetVercel } from "../../targets/vercel/index.js";
 
 export async function targetsPlugin(): Promise<PluginOption[] | undefined> {
@@ -10,11 +13,26 @@ export async function targetsPlugin(): Promise<PluginOption[] | undefined> {
   if (isPackageExists("vite-plugin-vercel")) {
     const vpv = await import("vite-plugin-vercel").then((p) => p.vercel());
 
+    let vikePrerenderOutdir: string | undefined;
+
     return [
       ...vpv,
+      {
+        name: "vike-photon:vercel:setup",
+        apply: "build",
+
+        buildStart: {
+          order: "pre",
+          handler() {
+            const api = getVercelAPI(this);
+            // Override `vite-plugin-vercel` config
+            api.defaultSupportsResponseStreaming = true;
+          },
+        },
+      },
       // Read +config values from +Page files, and compute `vercel` property on each entry
       {
-        name: "vike-photon:vercel",
+        name: "vike-photon:vercel:entries",
         apply: "build",
 
         applyToEnvironment(env) {
@@ -25,6 +43,57 @@ export async function targetsPlugin(): Promise<PluginOption[] | undefined> {
           handler() {
             setTargetVercel(this);
           },
+        },
+
+        sharedDuringBuild: true,
+      },
+      // Copy assets to vercel_client outDir
+      {
+        name: "vike-photon:vercel:prerender-read-config",
+        apply: "build",
+
+        applyToEnvironment(env) {
+          return env.name === "client";
+        },
+
+        closeBundle: {
+          order: "post",
+          handler() {
+            // Read outDir while in client env, where prerendering is executed
+            vikePrerenderOutdir = normalizePath(
+              path.isAbsolute(this.environment.config.build.outDir)
+                ? this.environment.config.build.outDir
+                : path.posix.join(this.environment.config.root, this.environment.config.build.outDir),
+            );
+          },
+        },
+
+        sharedDuringBuild: true,
+      },
+      {
+        name: "vike-photon:vercel:prerender",
+        apply: "build",
+
+        applyToEnvironment(env) {
+          return env.name === "vercel_client";
+        },
+
+        buildStart() {
+          const vikeConfig = getVikeConfig(this.environment.config);
+          if (vikeConfig?.prerenderContext?.output && vikePrerenderOutdir) {
+            for (const file of vikeConfig.prerenderContext.output) {
+              const is404 = Boolean(file.pageContext.is404);
+              const key = is404 ? "404.html" : normalizePath(file.filePath).substring(vikePrerenderOutdir.length + 1);
+
+              // Emit static HTML files in `vercel_client` env
+              this.emitFile({
+                type: "asset",
+                fileName: key,
+                originalFileName: key,
+                source: file.fileContent,
+              });
+            }
+          }
         },
 
         sharedDuringBuild: true,
