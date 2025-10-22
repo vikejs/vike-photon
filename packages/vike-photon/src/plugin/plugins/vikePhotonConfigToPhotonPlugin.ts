c6f0d06caac7dafe2e81de5db9b7e6fd34e33fb7
Romuald Brillout <git@brillout.com>
2025-10-22 17:03:43 +0200

    update usage

--- packages/vike-photon/src/plugin/plugins/vikePhotonConfigToPhotonPlugin.ts
+++ packages/vike-photon/src/plugin/plugins/vikePhotonConfigToPhotonPlugin.ts
@@ -23,13 +23,14 @@ export function vikePhotonConfigToPhotonPlugin(): Plugin {
           }
 
           if (!isAbsolute(serverPath)) {
-            const photonConfigUser:
-              | { definedAt?: { filePathAbsoluteFilesystem?: string }; value?: unknown }
-              | undefined = vikeConfig.ignoreWarning("_pageConfigGlobal")?.configValueSources?.photon?.[0];
+            const photonConfigUser = vikeConfig.dangerouslyUseInternals!._pageConfigGlobal.configValueSources.photon?.[0]
 
             if (
-              photonConfigUser?.definedAt?.filePathAbsoluteFilesystem &&
-              typeof photonConfigUser?.value === "object" &&
+              photonConfigUser &&
+              'filePathAbsoluteFilesystem' in photonConfigUser.definedAt &&
+              photonConfigUser.definedAt.filePathAbsoluteFilesystem &&
+              'value' in photonConfigUser &&
+              typeof photonConfigUser.value === "object" &&
               photonConfigUser.value &&
               "server" in photonConfigUser.value
             ) {
